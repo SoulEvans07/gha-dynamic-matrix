950ac07754893d84d59e07bd29427abcbec1c624
Adam Szi <adam.szi@tesco.com>
2023-12-19 18:38:31 +0100

    deploy line

---
+++ scripts/byRegion.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+echo "filter: $AFFECTED"
+
+CZ=("leaflet-be" "only-cz" "no-hu")
+HU=("leaflet-be" "voucher-fe")
+SK=("leaflet-be" "voucher-fe" "no-hu")
+
+# Filter elements in AFFECTED that are also in REGIONAL
+czAffected=()
+huAffected=()
+skAffected=()
+defaultAffected=()
+for element in ${AFFECTED[@]}; do
+  if [[ " ${CZ[@]} " =~ " $element " ]]; then
+    czAffected+=("\"$element\"")
+  elif [[ " ${HU[@]} " =~ " $element " ]]; then
+    huAffected+=("\"$element\"")
+  elif [[ " ${SK[@]} " =~ " $element " ]]; then
+    skAffected+=("\"$element\"")
+  else
+    defaultAffected+=("\"$element\"")
+  fi
+done
+
+echo "cz: ${czAffected[@]}"
+echo "hu: ${huAffected[@]}"
+echo "sk: ${skAffected[@]}"
+echo "default: ${defaultAffected[@]}"
+
+echo "toJSON"
+echo "cz: [$(echo ${czAffected[@]} | tr ' ' ',')]"
+echo "hu: [$(echo ${huAffected[@]} | tr ' ' ',')]"
+echo "sk: [$(echo ${skAffected[@]} | tr ' ' ',')]"
+echo "default: [$(echo ${defaultAffected[@]} | tr ' ' ',')]"
+
+echo "cz=${czAffected[@]}" >> $GITHUB_OUTPUT
+echo "hu=${huAffected[@]}" >> $GITHUB_OUTPUT
+echo "sk=${skAffected[@]}" >> $GITHUB_OUTPUT
+echo "cz_array=[$(echo ${czAffected[@]} | tr ' ' ',')]" >> $GITHUB_OUTPUT
+echo "hu_array=[$(echo ${huAffected[@]} | tr ' ' ',')]" >> $GITHUB_OUTPUT
+echo "sk_array=[$(echo ${skAffected[@]} | tr ' ' ',')]" >> $GITHUB_OUTPUT
+echo "default=[$(echo ${defaultAffected[@]} | tr ' ' ',')]" >> $GITHUB_OUTPUT
